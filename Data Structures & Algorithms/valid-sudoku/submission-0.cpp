8f8c430e1916da4e765c7857e6c39bb84f10452f
Sadiq <47571312+Sadiq0123@users.noreply.github.com>
2026-07-20 19:41:01 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<int> checkerRow(9, 0);
+        vector<int> checkerCol(9, 0);
+        vector<int> checkerSquare(9, 0);
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
+                if (board[i][j]=='.') continue;
+                int current = board[i][j] - '1';
+                if ((checkerRow[i] & (1 << current)) ||
+                (checkerCol[j] & (1 << current)) ||
+                (checkerSquare[(i/3)*3 + j/3] & (1 << current))) return false;
+
+                checkerRow[i] |= (1 << current);
+                checkerCol[j] |= (1 << current);
+                checkerSquare[(i/3)*3 + j/3] |= (1 << current);
+                // cout << "i: " << i << ", j: " << j << ", checker[i]: " << checker[i];
+            }
+        }
+        return true;
+    }
+};
