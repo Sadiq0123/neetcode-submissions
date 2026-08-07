8c9f1434fc5702639af2202b7fbf5fe7531f49b0
Sadiq <47571312+Sadiq0123@users.noreply.github.com>
2026-08-07 21:40:08 +0530

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n, -1);
+        for(int i=n-1; i>=0; i--) {
+            int next = i+1;
+            while(next<n) {
+                if (temperatures[next]>temperatures[i]) {
+                    ans[i] = next-i;
+                    break;
+                }
+                else if (ans[next]!=0) {
+                    next += ans[next];
+                }
+                else {
+                    next = n;
+                }
+            }
+            if (next==n) ans[i] = 0;
+        }
+        return ans;
+
+        // int arrMax = arrayMax(temperatures);
+        // stack<int> stk1, stk2;
+        // bool isStk1 = true;
+        // for(int i=0; i<n; i++) {
+        //     if(isStk1) {
+        //         isStk1 = false;
+        //         while(!stk1.empty()) {
+        //             int temp = stk1.top();
+        //             stk1.pop();
+        //             if(temperatures[temp]<temperatures[i]) {
+        //                 ans[temp] = i - temp;
+        //             } else stk2.push(temp);
+        //         }
+        //     } else {
+        //         isStk1 = true;
+        //         while(!stk2.empty()) {
+        //             int temp = stk2.top();
+        //             stk2.pop();
+        //             if(temperatures[temp]<temperatures[i]) {
+        //                 ans[temp] = i - temp;
+        //             } else stk1.push(temp);
+        //         }
+        //     }
+        //     if (temperatures[i]>=arrMax) {
+        //         ans[i] = 0;
+        //         continue;
+        //     }
+        //     if(isStk1) stk1.push(i);
+        //     else stk2.push(i);
+        // }
+        // if (isStk1) {
+        //     while(!stk1.empty()) {
+        //         int temp = stk1.top();
+        //         stk1.pop();
+        //         ans[temp] = 0;
+        //     }
+        // } else {
+        //     while(!stk2.empty()) {
+        //         int temp = stk2.top();
+        //         ans[temp] = 0;
+        //         stk2.pop();
+        //     }
+        // }
+        // return ans;
+    }
+
+    int arrayMax(vector<int> arr) {
+        if (arr.size()==0) return 0;
+        int maxx = arr[0];
+        for(int i=1; i<arr.size(); i++) {
+            maxx = max(maxx, arr[i]);
+        }
+        return maxx;
+    }
+};
